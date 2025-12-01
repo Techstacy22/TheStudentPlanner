cf8dcf826fba8246e0c6e0167688916071f35281
Techstacy22 <williamschisom.ecstacy@gmail.com>
2025-12-01 01:52:21 -0600

    Initial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,36 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/Navbar.css';
+import { FaBook, FaCalendarAlt, FaTasks, FaHome, FaRobot } from 'react-icons/fa';
+
 
 export default function Navbar() {
     return (
      <nav className='NavBar'>   
-     <div className='nav -left'>The Planner</div>                          
+     <div className='nav-left'>The Planner
+        <FaBook className='Nav-icon1' /> 
+    </div>                          
              <ul        className='nav-links'>
-                <li><Link to="/" >Home</Link></li>
-                <li><Link to="/email"> AI Email     </Link></li>
-                <li><Link to="/calendar" >Calendar</Link></li>
-                <li> <Link to="/tasks">Tasks</Link></li>
+                <li>
+                    <Link className='Nav-item' to="/" >Home
+                          <FaHome className='Nav-icon' />
+                    </Link>   
+                </li>
+                <li>    
+                    <Link className='Nav-item' to="/email"> AI Email    
+                        <FaRobot className='Nav-icon' />
+                    </Link>
+                 </li>
+                <li>    
+                    <Link className='Nav-item'to="/calendar" >Calendar  
+                        <FaCalendarAlt className='Nav-icon' />
+                    </Link>   
+                </li>
+                <li>
+                    <Link  className='Nav-item' to="/tasks">Tasks
+                        <FaTasks className='Nav-icon' />
+                    </Link>
+                </li>
                 
             </ul>
      </nav>
